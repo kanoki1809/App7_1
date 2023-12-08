37c8de27ab672acb53c87166830398f2e6e8555e
kanoki1809 <144712774+kanoki1809@users.noreply.github.com>
2023-12-08 10:42:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title('')
+input = open('lrc_xray.pkl','rb')
+model = pkl.load(input)
+
+st.header('1')
+gre = st.number_input('Insert GRE score')
+toefl=st.number_input('Insert torfl score')
+uni_rate = st.number_input('Insert UNI rate')
+sop = st.number_input('Insert SOP')
+lor =st.number_input('Insert Lor')
+cgpa=st.number_input('Insert CGPA')
+reseach= st.radio('choose reseach',)
